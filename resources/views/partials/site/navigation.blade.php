352da86232d3da1f0d9eec0af25f4c6acaf5b5b0
Pieter Passmann <pieter.passmann@azerion.com>
2022-09-20 10:24:18 +0200

    Navigation should conditionally load

--- resources/views/partials/site/navigation.blade.php
+++ resources/views/partials/site/navigation.blade.php
@@ -3,6 +3,7 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
+    @if(isset($pages))
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             @foreach($pages as $page)
@@ -19,4 +20,5 @@
             </li-->
         </ul>
     </div>
+    @endif
 </nav>
