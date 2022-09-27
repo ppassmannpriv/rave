9763b2a2562bc018e89f07b170086c365fa7a5fe
Pieter Passmann <pieter.passmann@azerion.com>
2022-09-27 22:28:18 +0200

    styling mostly

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -9,27 +9,27 @@
                 </div>
 
                 <div class="card-body">
-                    <span>Start: {{ $event->start }}</span>
-                    <span>End: {{ $event->end }}</span>
+                    <span>Start: {{ date('d-m-Y', strtotime($event->start)) }}</span><br />
+                    <span>End: {{ date('d-m-Y', strtotime($event->end)) }}</span><br />
                     <span>{{ $event->location }}</span>
-
-                    <ul class="list-group d-flex">
+                    <ul class="list-group d-flex mt-4">
                     @foreach($event->eventTickets as $eventTicket)
-                        <li class="list-group-item d-flex">
-                            <div class="col-sm-3"><span>{{ $eventTicket::TICKET_TYPE_RADIO[$eventTicket->ticket_type] }}</span></div>
-                            <div class="col-sm-3"><span>@money($eventTicket->price)</span></div>
+                        <li class="list-group-item d-flex flex-wrap">
+                            <div class="col-lg-3 col-sm-6 d-flex align-content-center flex-wrap"><span>{{ $eventTicket::TICKET_TYPE_RADIO[$eventTicket->ticket_type] }}</span></div>
+                            <div class="col-lg-3 col-sm-6 d-flex align-content-center flex-wrap"><span>@money($eventTicket->price)</span></div>
                             @if ($eventTicket->from || $eventTicket->to)
-                            <div class="col-sm-3"><span>from: {{ $eventTicket->from }}</span><br />
+                            <div class="col-lg-3 col-sm-12 d-flex align-content-center flex-wrap"><span>from: {{ $eventTicket->from }}</span><br />
                                 <span>to: {{ $eventTicket->to }}</span></div>
                             @else
-                            <div class="col-sm-3"></div>
+                            <div class="col-lg-3 col-sm-12 d-flex align-content-center flex-wrap"></div>
                             @endif
-                            <div class="col-sm-3">
-                                <form action="/cart/add/" method="POST">
+                            <div class="col-sm-12 d-md-none d-flex flex-wrap mt-3"></div>
+                            <div class="col-lg-3 col-sm-12 d-flex align-content-center flex-wrap">
+                                <form action="/cart/add/" method="POST" class="w-100">
                                     @csrf
                                     <input type="hidden" value="{{ $eventTicket->id }}" name="event_ticket_id" />
                                     <input type="hidden" value="1" name="qty-{{ $event->id }}" id="qty-{{ $event->id }}" />
-                                    <input type="submit" value="Add to Cart" />
+                                    <input type="submit" value="Buy" class="btn btn-outline-success w-100"/>
                                 </form>
                             </div>
                         </li>
