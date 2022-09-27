1e5a6ca24fc7e3f8a90d697dd8f07a0d31a18c01
Pieter Passmann <pieter.passmann@azerion.com>
2022-09-27 22:32:34 +0200

    styling mostly

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -12,7 +12,11 @@
                         <div class="col-lg-12">
                             <ul class="list-group">
                                 @foreach($events as $event)
-<li class="list-group-item">{{ $event->name }} <a href="{{ route('events.show', ['id' => $event->id]) }}/">Details</a></li>
+<li class="list-group-item flex-wrap d-flex">
+    <div class="col-sm-6 col-lg-12 d-flex"><h4>{{ $event->name }}</h4></div>
+    <div class="col-sm-6 col-lg-6 d-flex">{{ date('d-m-Y', strtotime($event->start)) }} - {{ date('d-m-Y', strtotime($event->end)) }}</div>
+    <div class="col-sm-6 col-lg-6 d-flex"><a href="{{ route('events.show', ['id' => $event->id]) }}/">Details</a></div>
+</li>
                                 @endforeach
                             </ul>
                         </div>
