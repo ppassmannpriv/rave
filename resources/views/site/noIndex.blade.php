99bff4d8b02dd1d3fe23813af1506fe491240b59
Pieter Passmann <pieter.passmann@azerion.com>
2022-10-18 22:48:09 +0200

    CSS

--- resources/views/site/noIndex.blade.php
+++ resources/views/site/noIndex.blade.php
@@ -4,7 +4,7 @@
 <div class="content">
     <div class="row">
         <div class="col-lg-12">
-            <div class="card">
+            <div class="card mb-0">
                 <div class="card-header">
                     No index page defined.
                 </div>
