074fce4669752f3eabd84bb8b69e3bd89a3a5818
Pieter Passmann <pieter.passmann@azerion.com>
2022-10-26 18:15:02 +0200

    Dashboard event

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,26 +4,9 @@
 <div class="content">
     <div class="row">
         <div class="col-lg-4">
-            <div class="card">
-                <div class="card-header">
-                    Dashboard
-                </div>
-
-                <div class="card-body">
-                    @if(session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4">
             @include('admin.partials.dashboard.nextEventStats')
         </div>
-        <div class="col-lg-12">
+        <div class="col-lg-8">
             @include('admin.partials.dashboard.orderWidget')
         </div>
     </div>
