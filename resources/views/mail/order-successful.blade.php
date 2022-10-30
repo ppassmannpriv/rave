8e581dc57a7094ea5f194771d4bce8e649170eaa
Pieter Passmann <pieter.passmann@azerion.com>
2022-10-30 14:14:27 +0100

    fix mail

--- resources/views/mail/order-successful.blade.php
+++ resources/views/mail/order-successful.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html>
-@include('mail.partials.head')
+@include('mail.partials.head', ['title' => 'Order #' . $order->id])
 <body style="background-color: #f6f6f6; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;">
 <span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">Order #{{ $order->id }}</span>
 <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f6f6f6; width: 100%;" width="100%" bgcolor="#f6f6f6">
