db89f9534f190e78688625dee34a2131e597fc23
Pieter Paßmann <pieter.passmann@gmail.com>
2024-01-26 13:33:34 +0100

    lower amount of segments per sms

--- app/Console/Commands/CheckEventSales.php
+++ app/Console/Commands/CheckEventSales.php
@@ -37,8 +37,8 @@ class CheckEventSales extends Command
         $events = Event::all()->filter(fn ($event) => !$event->isOver());
         $messages = [];
         foreach ($events as $event) {
-            $messages[] = sprintf("%s\nReserved: %d\nSold: %d\nEstimated income: %s",
-                $event->name,
+            $messages[] = sprintf("%s\nReserved: %d\nSold: %d\nIncome: %s",
+                $this->removeEmoji($event->name),
                 $event->eventTicketsReserved(),
                 $event->eventTicketsSold(),
                 env('CURRENCY_SYMBOL') . number_format($event->eventTicketsSoldPrice(), 2),
@@ -46,7 +46,7 @@ class CheckEventSales extends Command
         }
         $message = null;
         if (count($messages) > 0) {
-            $message = "Ticket update for " . Carbon::now()->toRfc822String() . "\n\n";
+            $message = "Ticket update " . Carbon::now()->format('d-m-Y H:i') . "\n\n";
             $message .= implode("\n\n", $messages);
         }
 
@@ -66,5 +66,38 @@ class CheckEventSales extends Command
         $this->info('Done.');
 
         return 0;
+    }
+
+    private function removeEmoji(string $string): string
+    {
+        // Match Enclosed Alphanumeric Supplement
+        $regex_alphanumeric = '/[\x{1F100}-\x{1F1FF}]/u';
+        $clear_string = preg_replace($regex_alphanumeric, '', $string);
+
+        // Match Miscellaneous Symbols and Pictographs
+        $regex_symbols = '/[\x{1F300}-\x{1F5FF}]/u';
+        $clear_string = preg_replace($regex_symbols, '', $clear_string);
+
+        // Match Emoticons
+        $regex_emoticons = '/[\x{1F600}-\x{1F64F}]/u';
+        $clear_string = preg_replace($regex_emoticons, '', $clear_string);
+
+        // Match Transport And Map Symbols
+        $regex_transport = '/[\x{1F680}-\x{1F6FF}]/u';
+        $clear_string = preg_replace($regex_transport, '', $clear_string);
+
+        // Match Supplemental Symbols and Pictographs
+        $regex_supplemental = '/[\x{1F900}-\x{1F9FF}]/u';
+        $clear_string = preg_replace($regex_supplemental, '', $clear_string);
+
+        // Match Miscellaneous Symbols
+        $regex_misc = '/[\x{2600}-\x{26FF}]/u';
+        $clear_string = preg_replace($regex_misc, '', $clear_string);
+
+        // Match Dingbats
+        $regex_dingbats = '/[\x{2700}-\x{27BF}]/u';
+        $clear_string = preg_replace($regex_dingbats, '', $clear_string);
+
+        return $clear_string;
     }
 }
