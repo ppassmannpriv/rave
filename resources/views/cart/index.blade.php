7a9a868fe8f1c95e5b1d20fd703cda6c89f0acfe
Pieter Paßmann <pieter.passmann@gmail.com>
2024-01-29 15:44:39 +0100

    viz fix

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -54,10 +54,10 @@
                             <div class="form-group mt-5">
                                 @include('partials.checkout.billing-address')
                                 <div class="d-flex container-fluid p-0 force-sm-block" style="display: block !important">
-                                    <div class="d-flex col-md-3 col-xs-12 p-0 padding-left-0 force-sm-block" style="display: block !important">
+                                    <div class="d-flex col-md-3 col-sm-12 p-0 padding-left-0 force-sm-block" style="display: block !important">
                                         @include('partials.checkout.payment-method')
                                     </div>
-                                    <ul id="totals" class="list-group container-fluid p-0 border col-md-3 col-xs-12 padding-left-0 force-sm-block justify-content-center" style="display: block !important">
+                                    <ul id="totals" class="list-group container-fluid p-0 border col-md-3 col-sm-12 padding-left-0 force-sm-block justify-content-center" style="display: block !important">
                                         <li class="list-group-item d-flex">
                                             <span class="d-flex price col-sm-8 text-left">Subtotal</span>
                                             <strong class="d-flex price col-sm-4 text-right">@money($cart->getSubTotal())</strong>
@@ -71,8 +71,8 @@
                                             <strong class="d-flex price col-sm-4 text-right">@money($cart->getTotal())</strong>
                                         </li>
                                     </ul>
-                                    <div class="col-md-4 col-xs-12 padding-left-0" style="display: block !important">
-                                        <ul id="legal-agreements" class="list-group container-fluid border col-xs-12 force-sm-block p-0" style="display: block !important">
+                                    <div class="col-md-4 col-sm-12 padding-left-0" style="display: block !important">
+                                        <ul id="legal-agreements" class="list-group container-fluid border col-sm-12 force-sm-block p-0" style="display: block !important">
                                             <li class="list-group-item d-flex ml-4">
                                                 <input id="general-terms-and-conditions" name="general-terms-and-conditions" type="checkbox" required value="1" class="form-check-input">
                                                 <label for="general-terms-and-conditions" class="form-check-label">I accept the general terms and conditions.</label>
@@ -82,7 +82,7 @@
                                                 <label for="data-protection-and-revocation" class="form-check-label">I have been informed about the data protection declaration and revocation information.</label>
                                             </li>
                                         </ul>
-                                        <button type="submit" class="btn btn-success col-xs-12 text-center mt-4 padding-left-0 force-sm-block" style="display: block !important">Order</button>
+                                        <button type="submit" class="btn btn-success col-sm-12 text-center mt-4 padding-left-0 force-sm-block" style="display: block !important">Order</button>
                                     </div>
                                 </div>
                             </div>
